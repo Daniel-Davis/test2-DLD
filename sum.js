03918c488672825b78239103f055ab01e88c332c
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-30 11:48:20 -0500

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,1 +1,14 @@
 // Daniel Davis, sum, takes the elements of an array and sums them up //
+
+var array = [3,1,4,1,5,9,2,6,5,4,5,1,3,9,5,7,9,3]; // holds array of numbers
+var total = 0; // holds total value
+
+print(total); // displays info
+print(array);
+
+for (var i=0, i < array.length, ++i) { // for loop to add them all up
+  total = total + [i]; // adds each element to the running total
+} // end of for loop
+
+print(array); // displays end result
+print(total);
