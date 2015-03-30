c2ce7621d9cf9ac221250d52a8f00920699f70bd
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-30 11:42:01 -0500

    Create array.js

---
+++ array.js
@@ -0,0 +1,17 @@
+// Daniel Davis, test2, holds all of the practicum code for second test //
+// question 3 - declare an array of data and sort it based on a certain condition //
+var pie = [3,1,4,1,5,9,2,6,5,4];
+function foo(array) {
+for (var i=0, i < array.length, ++i) {
+var first = [1];
+if([i] !== first) {
+[i] = 0;
+} // end of if statement
+else {
+[i] = 1;
+} // end of else
+} // end of for loop
+}// end of foo function
+print(pie);
+print(foo(pie));
+// end of question 3
