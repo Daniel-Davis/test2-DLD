500f0022318e832db696099505e56b869cf98933
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-30 12:03:51 -0500

    Update scope.js

--- scope.js
+++ scope.js
@@ -1,7 +1,12 @@
 //Daniel davis, scope, deminstrates javascript scope //
 
-var ice cream = 'rocky road';
+var ice cream = 'rockyRoad'; // global variable ice cream
 
-while(ice cream == 'rocky road') {
+for(var i=0, i < 7, i++) { // i make local variable i
+  ice cream = 'zombie flavored' // let's change the flavor for the shits and giggles
+} // end of for loop
+  
+  print(ice cream); // prints out ice cream flavor
+  print(i); // will not print because i is local variable and does not exist outsied of the forloop
   
 } // end of while loop
