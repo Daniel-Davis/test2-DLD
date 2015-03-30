ac7968d16ad73940768d054e124cbe37752591b8
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-30 11:51:48 -0500

    Update array.js

--- array.js
+++ array.js
@@ -1,10 +1,8 @@
 
 // Daniel Davis, test2, holds all of the practicum code for second test //
 // question 3 - declare an array of data and sort it based on a certain condition //
-load("")
+load("bubbleSort.js");
 var pie = [3,1,4,1,5,9,2,6,5,4]; // element of numbers
-
-
 print(pie);
-print(foo(pie));
+print(bubbleSort(pie));
 // end of question 3
